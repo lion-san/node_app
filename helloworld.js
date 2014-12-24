a50375231dbed37f8d8eff63d97ae79cfe38f8b1
yokoi.shinya <shinya.yokoi@gmail.com>
2014-12-24 12:43:39 +0900

    before 有益なものを提供する

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,5 @@
 console.log("Hello World");
+
+var test = 'http://localhost/';
+
+console.log(test.toString());
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,24 @@
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
-  return "Hello Start";
+  var content = "empty";
+
+  exec("ls -lah", function (error, stdout, stderr) {
+    content = stdout;
+  });
+
+  return content;
 }
+
+/*  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
+  return "Hello Start";
+}*/
 
 function upload() {
   console.log("Request handler 'upload' was called.");
